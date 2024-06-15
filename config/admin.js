be9d4c815b6517a0ae01020192e77012da931d41
AndresFcode <trabajoarfe7@gmail.com>
2024-06-15 08:48:41 -0400

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', ['LgHWRvKMddMtmE05tWtG+Q==']),
+    secret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT', ['ZnJR4BwF9yFLyV4yh4IAKA==']),
