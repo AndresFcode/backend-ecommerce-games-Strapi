4f160bcaa3c3fa9a71e3049b15ccb95805820ef3
AndresFcode <trabajoarfe7@gmail.com>
2024-06-15 03:49:37 -0400

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', ['LgHWRvKMddMtmE05tWtG+Q==']),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT', ['ZnJR4BwF9yFLyV4yh4IAKA==']),
