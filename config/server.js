3c713b4a2ecce7fbdb0c0c82808325a5f39c13d7
AndresFcode <trabajoarfe7@gmail.com>
2024-06-15 03:07:14 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['LqsZpPmu9S04bnypbZh5Iw==','hbWl7gpQ6o+fADit0r8L/A==','B6ubEJwCOlnNScJRlfn/8g==','iQO2TDVbPyhpxYsR/Ry3ng==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
